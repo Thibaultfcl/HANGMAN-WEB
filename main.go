438b1bd32772476cc78cc915767aeeb916f6e1a5
ThibaultF <thibault.frescaline@ynov.com>
2023-11-16 14:12:44 +0100

    hangmanweb

--- main.go
+++ main.go
@@ -15,10 +15,25 @@ func Rules(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "tmpl/rules.html")
 }
 
+func Leaderboard(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "tmpl/leaderboard.html")
+}
+
+func Game(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "tmpl/game.html")
+}
+
+func Profil(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "tmpl/profil.html")
+}
+
 func main() {
 
 	http.HandleFunc("/home", Home)
 	http.HandleFunc("/rules", Rules)
+	http.HandleFunc("/leaderboard", Leaderboard)
+	http.HandleFunc("/game", Game)
+	http.HandleFunc("/profil", Profil)
 
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
 	http.Handle("/img/", http.StripPrefix("/img/", http.FileServer(http.Dir("./img"))))
