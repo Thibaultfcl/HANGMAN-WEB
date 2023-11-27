5a6b80361099400d32c3bc6fe9d11b7d5a75c7eb
ThibaultF <thibault.frescaline@ynov.com>
2023-11-27 13:00:27 +0100

    hangman-web

--- functions/printHangman.go
+++ functions/printHangman.go
@@ -5,15 +5,24 @@ import (
 	"os"
 )
 
-func PrintHangman(attemptsLeft int) {
-	file, err := os.Open("hangman.txt")
-	if err != nil {
-		fmt.Println("Error:", err)
-		return
+func PrintHangman(attemptLeft int) string {
+	w := ""
+	if attemptLeft != 10 {
+		if Read("hangman.txt")[9] == byte(13) {
+			w = (string(Read("hangman.txt")[(9-attemptLeft)*79 : (9-attemptLeft)*79+77]))
+		} else {
+			w = (string(Read("hangman.txt")[(9-attemptLeft)*71 : (9-attemptLeft)*71+70]))
+		}
 	}
-	defer file.Close()
+	return w
+}
 
-	if attemptsLeft >= 1 && attemptsLeft <= 9 {
-		PrintLine(file, (9-attemptsLeft)*8, 7)
+func Read(s string) []byte {
+	file, err := os.Open(s)
+	if err != nil {
+		fmt.Println(err)
 	}
+	arr := make([]byte, 1200)
+	file.Read(arr)
+	return arr
 }
--- main.go
+++ main.go
@@ -2,10 +2,32 @@ package main
 
 import (
 	"fmt"
+	"hangman-web/functions"
+	"math/rand"
 	"net/http"
+	"os"
+	"strings"
+	"text/template"
 )
 
+type GameInfo struct {
+	AttemptLeft int
+	WordGuessed string
+	Hangman     string
+	Rep         []string
+}
+
 const port = ":8080"
+const redirect = 301
+
+var attemptLeft int
+var WordToGuess string // Mot (L A V A B O)
+var WordGuessed []rune // Mot (_ A _ _ B O)
+var bool = true
+
+var rep []string
+var Name string
+var level string
 
 func Home(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "tmpl/home.html")
@@ -19,21 +41,65 @@ func Leaderboard(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "tmpl/leaderboard.html")
 }
 
-func Game(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "tmpl/game.html")
-}
-
 func Profil(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "tmpl/profil.html")
 }
 
+func Game(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "tmpl/game.html")
+}
+
+func Game1(w http.ResponseWriter, r *http.Request) {
+	WordGuessed = []rune{}
+	if len(os.Args) == 1 {
+		if err := r.ParseForm(); err != nil {
+			fmt.Fprintf(w, "ParseForm() err: %v", err)
+			return
+		}
+		rep = []string{}
+		level = r.Form.Get("difficulty")
+		WordToGuess = strings.ToUpper(functions.Random(level))
+		Name = r.Form.Get("Pseudo")
+		attemptLeft = 10
+
+		//Mot caché (_ _ _ _ _)
+		for range WordToGuess {
+			WordGuessed = append(WordGuessed, '_')
+		}
+
+		//Premières lettres dévoilées (_ I _ _ E)
+		for v := 0; v < len(WordToGuess)/2-1; v++ {
+			random := rand.Intn(len(WordToGuess))
+			if WordGuessed[random] == '_' {
+				WordGuessed[random] = rune(WordToGuess[random])
+			} else {
+				v--
+			}
+		}
+	}
+	http.Redirect(w, r, "/", redirect)
+}
+
+func Redirect(w http.ResponseWriter, r *http.Request) {
+	if bool {
+		bool = false
+		http.Redirect(w, r, "/game", redirect)
+	} else {
+		new := GameInfo{AttemptLeft: attemptLeft, WordGuessed: string(WordGuessed), Hangman: functions.PrintHangman(attemptLeft), Rep: rep}
+		tmpl := template.Must(template.ParseFiles("tmpl/index.html"))
+		tmpl.Execute(w, new)
+	}
+}
+
 func main() {
 
+	http.HandleFunc("/", Redirect)
 	http.HandleFunc("/home", Home)
 	http.HandleFunc("/rules", Rules)
 	http.HandleFunc("/leaderboard", Leaderboard)
 	http.HandleFunc("/game", Game)
 	http.HandleFunc("/profil", Profil)
+	http.HandleFunc("/game1", Game1)
 
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
 	http.Handle("/img/", http.StripPrefix("/img/", http.FileServer(http.Dir("./img"))))
